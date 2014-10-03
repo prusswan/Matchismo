47f6ce53ad4b58a3bee505de3bafbe3a5759604e
prusswan <prusswan@gmail.com>
2014-10-03 14:58:47 +0800

    completed stub for CardGameViewController

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -9,18 +9,27 @@
 #import "CardGameViewController.h"
 
 @interface CardGameViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
 @end
 
 @implementation CardGameViewController
 
-
+- (void)setFlipCount:(int)flipCount
+{
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+    NSLog(@"flipCount = %d", self.flipCount);
+}
 - (IBAction)touchCardButton:(UIButton *)sender {
     if ([sender.currentTitle length]) {
-        
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"] forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+    } else {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"] forState:UIControlStateNormal];
+        [sender setTitle:@"A" forState:UIControlStateNormal];
     }
-    [sender setBackgroundImage:[UIImage imageNamed:@"cardback"] forState:UIControlStateNormal];
-    [sender setTitle:@"" forState:UIControlStateNormal];
+    self.flipCount++;
 }
 
 @end
